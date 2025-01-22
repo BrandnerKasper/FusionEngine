8653591283a719d58e65b66be532117de696c5fd
Tobias Brandner <tobias.brandner@gmx.de>
2025-01-22 14:51:43 +0100

    add tests

---
+++ tests/test_dataset.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include "../datasets/urban100.h"
+#include "../utils/utils.h"
+
+
+TEST(DataSet, GetImages) {
+    auto datasets = std::make_shared<datasets::Urban100>(std::string(PROJECT_ROOT_DIR) + "/data/Urban100/val");
+    for (int i = 0; i < datasets->size(); i++) {
+        auto sample = datasets->get(i);
+        auto data = sample.data;
+        auto target = sample.target;
+
+        std::cout << "Data: " << data << std::endl;
+        std::cout << "Target: " << target << std::endl;
+        utils::showTensorAsCVImg(data, target);
+    }
+}
