2f4bbfc8c6b12e3bb323e05d91cf8ca143e2fdf5
Tobias Brandner <tobias.brandner@gmx.de>
2025-08-05 15:19:46 +0200

    loading texture complete

--- LearnOpenGL/assets/shaders/fragment.frag
+++ LearnOpenGL/assets/shaders/fragment.frag
@@ -3,8 +3,12 @@ out vec4 fragColor;
 
 in vec3 ourColor;
 in vec4 ourPos;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture;
 
 void main() {
 //    fragColor = vec4(ourColor, 1.0);
-    fragColor = ourPos; // negative vals get clamped to 0.0 = black!
+//    fragColor = ourPos; // negative vals get clamped to 0.0 = black!
+    fragColor = texture(ourTexture, TexCoord);
 }
--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -5,19 +5,22 @@
 #include <glm/glm.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
+#include "Assets.h"
 #include "stb_image.h"
 #include "Shader.h"
 
-// Draw a triangle
+// Draw a rectangle
 float vertices[] = {
-    // positions        // colors
-     0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
-    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
-     0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // top
+    // positions        // colors         // texture coords
+     0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+    -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
 };
 
 unsigned int indices[] = {
-    0, 1, 2 // first triangle
+    0, 1, 2, // first triangle
+    2, 3, 0  // second triangle
 };
 
 float texCoords[] = {
@@ -82,15 +85,44 @@ void openGL() {
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // position attributes
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), static_cast<void *>(nullptr));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), static_cast<void *>(nullptr));
     glEnableVertexAttribArray(0);
 
     // color attributes
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), reinterpret_cast<void *>(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void *>(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    // texture attributes
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void *>(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     // uncomment this call to draw in wireframe polygons.
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // Texture stuff
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping/filtering options (on currently bound texture)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(assets::path("textures/container.jpg").c_str(), &width, &height,
+    &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+        GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
 
     // render loop
     while(!glfwWindowShouldClose(window)) {
