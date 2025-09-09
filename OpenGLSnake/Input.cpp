5481d16d041d4fdfdf0ac7281f05af83c97a4abf
Tobias Brandner <tobias.brandner@gmx.de>
2025-09-09 13:36:15 +0200

    ask for hold and pressed

--- OpenGLSnake/Input.cpp
+++ OpenGLSnake/Input.cpp
@@ -4,5 +4,9 @@
 bool Input::pressed(const Action a) {
     if (!m_window)
         return false;
-    return glfwGetKey(m_window, m_bindings[a]) == GLFW_PRESS;
+    const bool pressed = glfwGetKey(m_window, m_bindings[a]) == GLFW_PRESS;
+    const bool hold = glfwGetKey(m_window, m_bindings[a]) == GLFW_REPEAT;
+    // bool released = glfwGetKey(m_window, m_bindings[a]) == GLFW_RELEASE;
+
+    return pressed || hold;
 }
