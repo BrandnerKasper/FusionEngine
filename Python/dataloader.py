96c3f1befb522fd297e056bbf0d5dab724fc72a4
Tobias Brandner <tobias.brandner@gmx.de>
2025-09-23 11:41:59 +0200

    rafactor dataloader to use utility fcts.

--- Python/dataloader.py
+++ Python/dataloader.py
@@ -1,10 +1,10 @@
 import os
-import cv2
 import torch
 from torch.utils.data import Dataset
-from torchvision import transforms
 import matplotlib.pyplot as plt
 from torchvision.transforms import functional as FV
+
+from utility import load_txt, load_img, tensor_to_ascii
 
 
 class ASCIISnake(Dataset):
@@ -59,37 +59,6 @@ class ASCIISnake(Dataset):
 
         plt.tight_layout()
         plt.show()
-
-# utility
-char_map = {
-    ' ': 0,
-    '#': 1,
-    'o': 2,
-    '•': 3
-}
-
-def load_txt(path: str) -> torch.Tensor:
-    with open(f"{path}.txt", "r", encoding="utf-8") as file:
-        lines = [line.rstrip("\n") for line in file]
-    grid = [[char_map.get(c) for c in line] for line in lines]
-    return torch.tensor(grid, dtype=torch.float32)
-
-
-def load_img(path: str) -> torch.Tensor:
-    img = cv2.imread(f"{path}.png", cv2.IMREAD_UNCHANGED)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    transform = transforms.ToTensor()
-    return transform(img)
-
-
-id_to_char = {v: k for k, v in char_map.items()}
-
-def tensor_to_ascii(grid: torch.Tensor) -> str:
-    lines = []
-    for row in grid.tolist():
-        chars = [id_to_char.get(val, '?') for val in row]
-        lines.append(''.join(chars))
-    return '\n'.join(lines)
 
 
 def main() -> None:
