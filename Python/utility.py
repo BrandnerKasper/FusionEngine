13edc2397f03fafbe5d5d07d5be642d7acb61b11
Tobias Brandner <tobias.brandner@gmx.de>
2025-09-24 13:04:30 +0200

    add one hot encoding

--- Python/utility.py
+++ Python/utility.py
@@ -1,7 +1,7 @@
 import torch
 import cv2
 from torchvision import transforms
-
+import torch.nn.functional as F
 
 # Utility fct. to turn txt, img files into tensors and back
 
@@ -19,6 +19,17 @@ def load_txt(path: str) -> torch.Tensor:
     return torch.tensor(grid, dtype=torch.float32).unsqueeze(0)
 
 
+K = len(char_map)
+def load_txt_one_hot_encode(path: str) -> torch.Tensor:
+    with open(f"{path}.txt", "r", encoding="utf-8") as file:
+        lines = [line.rstrip("\n") for line in file]
+    grid = [[char_map.get(c) for c in line] for line in lines]
+    grid = torch.tensor(grid, dtype=torch.long)
+    one_hot = F.one_hot(grid, num_classes=K).permute(2, 0 , 1).to(torch.float32)
+    return one_hot
+
+
+
 def load_img(path: str) -> torch.Tensor:
     img = cv2.imread(f"{path}.png", cv2.IMREAD_UNCHANGED)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -28,9 +39,15 @@ def load_img(path: str) -> torch.Tensor:
 
 id_to_char = {v: k for k, v in char_map.items()}
 
-def tensor_to_ascii(grid: torch.Tensor) -> str:
+def grid_to_ascii(grid: torch.Tensor) -> str:
     lines = []
     for row in grid.squeeze(0).tolist():
         chars = [id_to_char.get(val, '?') for val in row]
         lines.append(''.join(chars))
+    return '\n'.join(lines)
+
+
+def one_hot_grid_to_ascii(one_hot: torch.Tensor) -> str:
+    ids = one_hot.argmax(dim=0)
+    lines = [''.join(id_to_char.get(int(v), '?') for v in row) for row in ids.tolist()]
     return '\n'.join(lines)
